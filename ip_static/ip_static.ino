77e63da505958565391b494a10aa780a906f1668
Guilherme de Oliveira Antonio <guilherme.oliveira.antonio@gmail.com>
2017-02-11 23:33:30 -0200

    Programa~

---
+++ ip_static/ip_static.ino
@@ -0,0 +1,23 @@
+#include <ESP8266WiFi.h>
+
+void setup()
+{
+// config static IP
+// where xx is the desired IP Address
+IPAddress ip(192, 168, 1, 99); 
+// set gateway to match your network
+IPAddress gateway(192, 168, 1, 1); 
+Serial.print(F("Setting static ip to : "));
+Serial.println(ip);
+// set subnet mask to match your network
+IPAddress subnet(255, 255, 255, 0);
+WiFi.config(ip, gateway, subnet);
+  pinMode(16, OUTPUT);
+}
+void loop()
+{
+    digitalWrite(16, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+  digitalWrite(16, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);              // wait for a second
+} 
